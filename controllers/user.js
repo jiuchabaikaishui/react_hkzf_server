02ac588be430f409952c1388c5d6a24d2a888115
QSP <QSP@163.com>
2025-04-09 11:36:58 +0800

    调整

--- controllers/user.js
+++ controllers/user.js
@@ -70,7 +70,7 @@ module.exports = {
             where: {
                 userName: username,
                 userPass: password,
-                userLock: false
+                // userLock: false
             }
         });
 
